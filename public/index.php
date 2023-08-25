823f66659227849084ac91473dd80626dbdef027
Николай Рябинин <rnn2002@mail.ru>
2023-08-25 19:22:38 +0300

    Add public/index.php

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hexlet\Code;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($request, $response) {
+    $flash = $this->get('flash')->getMessages();
+    return $response->write('Тут будет главная страница проекта "Анализатор страниц"');
+})->setName('main');
+
+$app->run();
