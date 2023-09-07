fc7894b8448636aafbd61c30de587cccaff1f91d
Николай Рябинин <rnn2002@mail.ru>
2023-09-07 21:17:47 +0300

    Modify  public/index.php

--- public/index.php
+++ public/index.php
@@ -25,37 +25,4 @@ $app->get('/', function ($request, $response) {
         return $this->get('renderer')->render($response, 'main.phtml');
 })->setName('main');
 
-/**
- * GET /{file} - Load a static asset.
- *
- * THIS MUST BE PLACED AT THE VERY BOTTOM OF YOUR SLIM APPLICATION, JUST BEFORE
- * $app->run()!!!
- */
-$app->get('/{file}', function ($request, $response, $args) {
-        $filePath = __DIR__ . '/' . $args['file'];
-
-        if (!file_exists($filePath)) {
-            return $response->withStatus(404, 'File Not Found');
-        }
-
-        switch (pathinfo($filePath, PATHINFO_EXTENSION)) {
-            case 'css':
-                $mimeType = 'text/css';
-                break;
-
-            case 'js':
-                $mimeType = 'application/javascript';
-                break;
-
-            default:
-                $mimeType = 'text/html';
-        }
-
-        $newResponse = $response->withHeader('Content-Type', $mimeType . '; charset=UTF-8');
-
-        $newResponse->getBody()->write(file_get_contents($filePath));
-
-        return $newResponse;
-    });
-
 $app->run();
