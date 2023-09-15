2a432a05da72c8be36adcc27926f1d49d4e0a757
Николай Рябинин <rnn2002@mail.ru>
2023-09-15 16:19:55 +0300

    Modify public/index.php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Flash;
 use Carbon\Carbon;
 use Valitron\Validator;
-use Database;
+use Hexlet\Code\Database;
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -17,6 +17,10 @@ $container->set('renderer', function () {
 $container->set('flash', function () {
         return new \Slim\Flash\Messages();
 });
+$container->set('pdo', function () {
+        $pdo = Database::get()->connect();
+        return $pdo;
+});
 
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
@@ -24,6 +28,20 @@ $app->add(MethodOverrideMiddleware::class);
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
+        $this->get('pdo')->exec("CREATE TABLE IF NOT EXISTS urls (
+                id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                name varchar(255) NOT NULL UNIQUE,
+                created_at timestamp
+            );");
+        $this->get('pdo')->exec("CREATE TABLE IF NOT EXISTS url_checks (
+                id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                url_id bigint REFERENCES urls (id),
+                status_code int,
+                h1 text,
+                title text,
+                description text,
+                created_at timestamp
+            );");
         return $this->get('renderer')->render($response, 'main.phtml');
 })->setName('main');
 
