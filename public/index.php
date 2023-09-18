20a24dccf3114557f7ced077429009359ce7e36a
Николай Рябинин <rnn2002@mail.ru>
2023-09-18 11:38:56 +0400

    Modify public/index.php

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ use Carbon\Carbon;
 use Valitron\Validator;
 use Hexlet\Code\Database;
 
+session_start();
+
 $container = new Container();
 $container->set('renderer', function () {
         return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -44,5 +46,53 @@ $app->get('/', function ($request, $response) {
             );");
         return $this->get('renderer')->render($response, 'main.phtml');
 })->setName('main');
+
+$app->post('/urls', function ($request, $response) use ($router) {
+    $rawData = (array)$request->getParsedBody();
+    $v = new Validator($rawData['url']);
+    $v->rule('required', 'name')->message('URL не должен быть пустым')
+        ->rule('url', 'name')->message('Некорректный URL')
+        ->rule('lengthMax', 'name', 255)->message('Некорректный URL');
+
+    if (!($v->validate())) {
+        $errors = $v->errors();
+        $params = [
+            'url' => $rawData['url'],
+            'errors' => $errors,
+            'isInvalid' => 'is-invalid',
+        ];
+
+        return $this->get('renderer')->render($response->withStatus(422), 'main.phtml', $params);
+    }
+
+    try {
+        $pdo = $this->get('pdo');
+
+        $urlString = strtolower($rawData['url']['name']);
+        $parsedUrl = parse_url($urlString);
+        $name = "{$parsedUrl['scheme']}://{$parsedUrl['host']}";
+        $createdAt = Carbon::now();
+
+        $query = "SELECT name FROM urls WHERE name = '{$name}'";
+        $existedUrl = $pdo->query($query)->fetchAll();
+
+        if (count($existedUrl) > 0) {
+            $query = "SELECT id FROM urls WHERE name = '{$name}'";
+            $existedUrlId = (string)($pdo->query($query)->fetchColumn());
+
+            $this->get('flash')->addMessage('success', 'Страница уже существует');
+            return $response->withRedirect($router->urlFor('show', ['id' => $existedUrlId]));
+        }
+
+        $query = "INSERT INTO urls (name, created_at) VALUES ('{$name}', '{$createdAt}')";
+        $pdo->exec($query);
+        $lastId = $pdo->lastInsertId();
+
+        $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
+        return $response->withRedirect($router->urlFor('show', ['id' => $lastId]));
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
+    }
+});
 
 $app->run();
